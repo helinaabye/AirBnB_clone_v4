b4a1516e7a4c037f1a34c4fa7565fd30ebe43dc5
Yaekob Demisse <94002610+Jamescog@users.noreply.github.com>
2022-11-27 14:34:35 +0300

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,17 +3,14 @@ $(document).ready(function(){
   $('input[type=checkbox]').change(function () {
     if ($(this).prop('checked')) {
       selected.push($(this).attr('data-name'))
-      console.log(selected)
     } else {
       let unchecked = $(this).attr('data-name')
       let idx = selected.indexOf(unchecked)
       selected.splice(idx, 1)
-      console.log(selected)
     }
   
   if (selected.length !== 0) {
     let seleced_in_txt = selected.join(", ")
-    console.log(seleced_in_txt)
     $("#selected").text(seleced_in_txt)
   }
 })
