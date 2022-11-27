a8897e16aafeebc1faa3d8199b082ac819137698
Jamescog <jamescog72@gmail.com>
2022-11-27 14:46:00 +0300

    change

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,15 @@
 $(document).ready(function(){
-  let selected = []
-  $('input[type=checkbox]').change(function () {
-    if ($(this).prop('checked')) {
-      selected.push($(this).attr('data-name'))
-      console.log(selected)
+  let selected  = []
+  $('input[type="checkbox"').change(function(){
+    if ($(this).prop("checked")) {
+      selected.push($(this).attr("data-name"))
     } else {
-      let unchecked = $(this).attr('data-name')
-      let idx = selected.indexOf(unchecked)
+      let removed = $(this).attr("data-name")
+      let idx  = selected.indexOf(removed)
       selected.splice(idx, 1)
-      console.log(selected)
     }
-  
-  if (selected.length !== 0) {
-    let seleced_in_txt = selected.join(", ")
-    console.log(seleced_in_txt)
-    $("#selected").text(seleced_in_txt)
-  }
-})
+  console.log(selected)
+  let intext  = selected.join(", ")
+  $("#selected").text(intext)
+  })
 })
